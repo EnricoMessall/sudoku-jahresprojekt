672176b316e15ca417da3f0c2cf019efc175d8f2
Nicolas Goldack <nicolas-goldack@live.de>
2020-05-16 15:22:51 +0200

    Interface for gui updated

--- src/gui.h
+++ src/gui.h
@@ -1,6 +1,38 @@
 #include <commands.h>
 #include <base.h>
 
+#define CHANGESTATE_NOTHING 0
+#define CHANGESTATE_GAME_NEW 1
+#define CHANGESTATE_GAME_QUIT 2
+#define CHANGESTATE_REQUEST_HINT 3
+#define CHANGESTATE_REQUEST_DESCRIPTION 4
+
+typedef struct fieldChange {
+    int location[2];
+    int change;
+} FieldChange;
+
+typedef struct change {
+    int changeState;
+    FieldChange fieldChange;
+} Change;
+
 void printField(Sudoku sudoku);
 
+void printLeaderboard(LeaderBoardElement *leaderBoardElement);
+
 void printMenu(int state);
+
+/*
+    Description:
+        Asks the user for a difficulty
+    Value:
+        The difficulty
+*/
+int getDifficulty(void);
+
+/*
+    Description:
+        Returns the Change
+*/
+Change getChange(void);
