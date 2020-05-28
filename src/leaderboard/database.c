2bfa19229ddc8458ac72a595892a13a08c86d684
Selman <sel.bdk@gmail.com>
2020-05-28 11:15:21 +0200

    Move function comments above it's prototype declarations

--- src/leaderboard/database.c
+++ src/leaderboard/database.c
@@ -8,21 +8,16 @@
 // Global Array of LeaderBoardElement Items, scoped to this file only
 static LeaderBoardElement elements[MAX_LEADERBOARD_ELEMENTS];
 
-// Creates a SQLite3 Sudoku.db table based on the passed sql statement,
-// as long as the Database Connection State did not encounter any Errors before.
 void createTableIfNotExists(SQLite3Context db, const char *sql) {
     if (db.connection_state == SQLITE_OK)
         db.connection_state = sqlite3_exec(db.connection, sql, NULL, NULL, &db.err_msg);
 }
 
-// Inserts a Row into a Sudoku.db table, as long as the Database Connection State did not encounter any Errors before.
 void insertIntoTable(SQLite3Context db, const char *sql) {
     if (db.connection_state == SQLITE_OK)
         db.connection_state = sqlite3_exec(db.connection, sql, NULL, NULL, &db.err_msg);
 }
 
-// Selects all the Records from the SQLite3 Sudoku.db based on the provided difficulty and as the Database Connection State did not encounter any Errors before.
-// Returns either a ordered Array of LeaderBoardElement Items or a NULL on Error to the Database Connection State.
 LeaderBoardElement *selectRecords(SQLite3Context db, int difficulty) {
     // Reset array
     memset(elements, 0, MAX_LEADERBOARD_ELEMENTS * (sizeof(elements[0])));
@@ -34,8 +29,6 @@ LeaderBoardElement *selectRecords(SQLite3Context db, int difficulty) {
     return elements;
 }
 
-// SQLite3 Callback Function.
-// Called multiple times in the caller Function, for each resultung row in the SELECT Statement.
 int callback(void *NotUsed, int nColumns, char **strArrRows,
              char **strArrColNames) {
     LeaderBoardElement element;
@@ -50,8 +43,6 @@ int callback(void *NotUsed, int nColumns, char **strArrRows,
     return 0;
 }
 
-// Adds an LeaderBoardElement to the global static LeaderBoardElement Array named 'elements'.
-// This Function is also only scoped to this File and keeps its changed State through each invocation.
 static void add(LeaderBoardElement element) {
     static int i = 0;
     // Reset i if Array is Empty
